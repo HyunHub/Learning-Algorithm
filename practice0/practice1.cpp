04293373e0250397d4c89223f98923f5d07bd39a
HyunHub <19924___@naver.com>
2021-04-01 13:27:37 +0900

    Practice 1

---
+++ practice0/practice1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//1. 1부터 N까지의 M의 배수 합 
+
+int main() {
+	int n, m, i, sum =0;
+	cin>> n >> m;
+	
+	for(i = 1; i<= n; i++) {
+		if(i%m == 0) {
+			sum += i;
+		}
+		
+	}
+	
+	cout << sum;
+	return 0;
+	
+}

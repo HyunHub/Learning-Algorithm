c2be156557549b209956d7cc200a0aff02cbd4a5
HyunHub <19924___@naver.com>
2021-04-01 14:30:58 +0900

    Practice 4

---
+++ practice0/practice4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// 나이 차이
+ 
+int main(){
+	
+	// ReadText 
+	freopen("input.txt", "rt", stdin);
+	int n, i, a, max = -2147000000, min = 2147000000;
+	
+	cin >> n;
+	for(i=1; i <= n; i++) {
+		cin >> a;
+		if(a > max) max =a;
+		if(a < min) min = a;
+	}
+
+	cout << "max age - min age = " << max - min << endl;
+	
+	return 0;
+}

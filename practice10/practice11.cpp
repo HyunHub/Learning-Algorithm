48391b1959069435bbd8f044a73b657a3c553e87
HyunHub <19924___@naver.com>
2021-04-03 13:02:53 +0900

    Practice 11

--- practice10/practice11.cpp
+++ practice10/practice11.cpp
@@ -1,1 +1,22 @@
+#include <stdio.h>
+using namespace std;
 
+// 숫자의 총 개수(small)
+
+int main() {
+	
+	int num, sum=0, tmp;
+	
+	scanf("%d", &num);
+	
+	for(int i=1; i<=num; i++) {
+		tmp = i;
+		while(tmp >0) {
+			tmp = tmp/10;
+			sum++;
+		}
+	}
+	
+	printf("%d", sum);
+	return 0;
+} 

7e0b1a76cd74a9b45f7e393c112fa77332c2fea3
HyunHub <19924___@naver.com>
2021-04-08 13:43:47 +0900

    Practice 23

---
+++ practice20/practice23.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// 연속 부분 증가 수열 
+
+int main() {
+	
+	int n, pre, now, i, cnt, max;
+	
+	scanf("%d", &n);
+	scanf("%d" , &pre);
+	
+	cnt= 1;
+	max= 1;
+	
+	for(i=1; i<n; i++) {
+		scanf("%d", &now);
+		if(pre <= now) {
+			cnt++;
+			if(cnt > max) max=cnt;
+		}
+		else cnt=1;
+		pre = now;
+	}
+	
+	printf("%d",max);
+	
+	return 0;
+	
+} 

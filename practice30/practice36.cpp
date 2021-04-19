3af6ebec3cfde6119a447453aa57b3a9ecf25a70
HyunHub <19924___@naver.com>
2021-04-19 17:42:44 +0900

    Practice 36

--- practice30/practice36.cpp
+++ practice30/practice36.cpp
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <vector>
+#include <algorithm>
 
-// insertion sort 삽입 정렬 
+// insertion sort 
 
 int main() {
 	
+	int a[100], n, tmp, i, j;
+	
+	scanf("%d", &n);
+	for(i=0; i<n; i++) {
+		scanf("%d", &a[i]);
+	}
+	
+	for(i=1; i<n; i++) {
+		tmp = a[i];
+		for(j= i-1; j>=0; j--) {
+			if(a[j] > tmp) a[j+1] = a[j];
+			else break;
+		}
+		a[j+1] = tmp;
+	}
+	
+	for(i=0; i<n; i++) {
+		printf("%d ", a[i]);
+	}
+	return 0;
 	
 }

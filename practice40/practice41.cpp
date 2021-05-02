2885f14d55c8e9da44b209eb8d3894898962b312
HyunHub <19924___@naver.com>
2021-05-02 14:57:55 +0900

    Practice 41

---
+++ practice40/practice41.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 연속된 자연수의 합
+ 
+int main() {
+	
+	int a, b=1, cnt=0, tmp, i;
+	
+	scanf("%d", &a); //15
+	 
+	tmp = a;
+	
+	a--; // 14
+	while(a>0) {
+		b++; // 2  3
+		a= a-b; // 12  9
+		if(a % b == 0) { // o  o  
+			for(i=1; i<b; i++) {
+				printf("%d + ", (a/b)+i); // 7  4 + 5
+			}
+			printf("%d = %d\n", (a/b)+i, tmp); // 8  6
+			cnt++;
+		} 
+	} 
+	 
+	return 0;
+	
+}

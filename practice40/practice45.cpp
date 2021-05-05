65e422675ac282783a689ab23503c472a6e027c7
HyunHub <19924___@naver.com>
2021-05-05 21:36:26 +0900

    Practice 45

--- practice40/practice45.cpp
+++ practice40/practice45.cpp
@@ -3,8 +3,35 @@
 #include <algorithm>
 using namespace std;
 
-// 공주 구하기 
+// 공주 구하기 (조세퍼스)  
 
 int main() {
 	
+	int n, k, i, pos=0, bp=0, cnt=0;
+	
+	scanf("%d %d", &n, &k);
+	vector<int> p(n+1);
+	
+	while(1) {
+		pos++;
+		if(pos >n) pos=1;
+		if(p[pos]==0) {
+			cnt++;
+			if(cnt == k) {
+				p[pos] =1;
+				cnt=0;
+				bp++;
+			}
+		}
+		if(bp == n-1) break;
+	}
+	
+	for(i=1; i<=n; i++) {
+		if(p[i] == 0) {
+			printf("%d\n", i);
+			break;
+		}
+	}
+	
+	return 0;
 } 

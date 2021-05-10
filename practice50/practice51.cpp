a6a56f4e7cd4225bc543d9fce32eb4115f114190
HyunHub <19924___@naver.com>
2021-05-10 18:27:39 +0900

    Practice 51

---
+++ practice50/practice51.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 영지(territory) 선택 : large
+// 2차원 배열 구간합 : 제한시간 1초 : Dynamic Program 
+
+int a[701][701], dy[701][701];
+int main() {
+	freopen("input.txt", "rt", stdin);
+	int h, w, n, m, i, j, tmp, max=-2147000000;
+	scanf("%d %d", &h, &w);
+	
+	for(i=1; i<=h; i++) {
+		for(j=1; j<=w; j++) {
+			scanf("%d", &a[i][j]);
+			dy[i][j] = dy[i-1][j]+dy[i][j-1]-dy[i-1][j-1]+a[i][j];
+		}
+	}
+	
+	scanf("%d %d", &n, &m);
+	for(i=n; i<=h; i++) {
+		for(j=m; j<=w; j++) {
+			tmp=dy[i][j]-dy[i-n][j]-dy[i][j-m]+dy[i-n][j-m];
+			if(tmp>max) max=tmp;
+		}
+	}
+	
+	printf("%d\n", max);
+	return 0;
+}

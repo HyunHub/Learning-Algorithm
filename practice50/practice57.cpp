617f5863adb4cb1c04afcd6b3fa21f5577af581d
HyunHub <19924___@naver.com>
2021-05-14 17:57:06 +0900

    Practice 57

---
+++ practice50/practice57.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include<stack> 
+using namespace std;
+
+// 재귀함수 이진수 출력
+ 
+void D(int x) {
+	
+	if(x==0) return;
+	else {
+		D(x/2);
+		printf("%d", x%2);
+	}
+}
+
+int main() {
+	
+	int n;
+	scanf("%d", &n);
+	D(n);
+	return 0;
+}

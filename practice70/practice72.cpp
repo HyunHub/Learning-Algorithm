6099aec53a582844e5af42b6d1fefd3a5c130429
HyunHub <19924___@naver.com>
2021-05-26 15:51:43 +0900

    Practice 72

--- practice70/practice72.cpp
+++ practice70/practice72.cpp
@@ -4,6 +4,28 @@
 #include <queue>
 using namespace std;
 
+// 공주 구하기(queue)
+ 
 int main() {
 	
+	int n, k, i;
+	queue<int> Q;
+	scanf("%d %d", &n, &k);
+	for(i=1; i<=n; i++) {
+		Q.push(i);
+	}
+	
+	while(!Q.empty()) {
+		for(i=1; i<k; i++) {
+			Q.push(Q.front());
+			Q.pop();
+		}
+		Q.pop();
+		if(Q.size()==1) {
+			printf("%d\n", Q.front());
+			Q.pop();
+		}
+	}
+	
+	return 0;
 }

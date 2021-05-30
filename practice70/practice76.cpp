9b887bd8076516c9537e5f31267239fd0d8ff7bb
HyunHub <19924___@naver.com>
2021-05-30 13:21:22 +0900

    Practice 76

--- practice70/practice76.cpp
+++ practice70/practice76.cpp
@@ -5,7 +5,37 @@
 using namespace std;
 
 // 이항계수(메모이제이션)
- 
+
+int dy[21][21]; // 배열에 저장해둬서 또 계산하지 않게  
+int DFS(int n, int r) {
+	if(dy[n][r]>0) return dy[n][r];
+	if(n==r || r==0) return 1;
+	else return dy[n][r]=DFS(n-1, r-1) + DFS(n-1, r);
+}
 int main() {
 	
+//	int n, r, res=1;
+//	
+//	scanf("%d %d", &n, &r);
+//	
+//	int v = n-r;
+//	
+//	if(n<r) return 0;
+//	
+//	while(r>0) {
+//		while(n>v){
+//			res *= n;
+//			n--;
+//		}
+//		res /= r;
+//		r--;
+//	} 
+//	
+//	printf("%d", res);
+
+	int n, r;
+	scanf("%d %d", &n, &r);
+	printf("%d\n", DFS(n, r));
+	
+	return 0;
 }

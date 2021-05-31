ab4f619ce54ee9550ac033a1a6866374d204525a
HyunHub <19924___@naver.com>
2021-05-31 13:11:03 +0900

    Practice 78

---
+++ practice70/practice78.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <queue>
+#include <vector>
+#include <algorithm> 
+// #include <iostream>
+using namespace std;
+
+// 원더랜드(Kruskal MST 알고리즘 : Union&Find 활용) 
+ 
+int main() {
+	
+} 

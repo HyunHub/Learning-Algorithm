874a3f232f7a9320af9592037f2f5a9ad3ce0152
HyunHub <19924___@naver.com>
2021-06-26 17:06:26 +0900

    STL map - char

--- practice_dp/practice_dp1.cpp
+++ practice_dp/practice_dp1.cpp
@@ -1,11 +1,34 @@
-#include <stdio.h>
-#include <queue>
-#include <vector>
+//#include <stdio.h>
+//#include <queue>
+//#include <vector>
 //#include <algorithm> 
-// #include <iostream>
+#include <iostream>
 //#include<bits/stdc++.h> 
+#include <fstream>
+#include <map>
 using namespace std;
 
 int main() {
 	
+	ios_base::sync_with_stdio(false);
+	// ifstream cin;
+	// cin.open("input.txt");
+	
+	map<char, int> ch; // 균형잡힌 2진 트리로 생성  
+	map<char, int>::iterator it; // map 접근 가능
+	char a[100];
+	cin >> a;
+	for(int i=0; a[i]!='\0'; i++){
+		ch[a[i]]++;
+	}
+	
+	//ch.end()는 마지막 자료의 바로 뒤 가리킴  
+	for(it=ch.begin(); it!=ch.end(); it++){
+		cout<<it->first<<" "<<it->second<< "\n";
+	}
+	
+	
+	return 0;
 }
+
+

26b8209eb33dd6586adccd1e8c6d0c6724b239ef
HyunHub <19924___@naver.com>
2021-06-29 21:52:36 +0900

    Bottom-Up

--- practice_dp/practice_dp3.cpp
+++ practice_dp/practice_dp3.cpp
@@ -2,15 +2,29 @@
 //#include <queue>
 //#include <vector>
 //#include <algorithm> 
-#include <iostream>
-//#include<bits/stdc++.h> 
-#include <fstream>
-#include <map>
-#include <string> 
+//#include <iostream>
+#include<bits/stdc++.h> 
+//#include <fstream>
+//#include <map>
+//#include <string> 
 using namespace std;
 
-// 네트워크 선 자르기 
- 
+// 네트워크 선 자르기 Bottom-Up 
+
+int dy[50];
 int main() {
 	
+	ios_base::sync_with_stdio(false);
+	
+	int n;
+	cin>>n;
+	dy[1]=1;
+	dy[2]=2;
+	for(int i=3; i<=n; i++){
+		dy[i] = dy[i-2]+dy[i-1];
+	}
+	
+	cout<<dy[n];
+	
+	return 0;
 }

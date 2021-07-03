cdd204d3e0cadbdb8afa0cd3080c0773fde445a4
HyunHub <19924___@naver.com>
2021-07-03 19:39:02 +0900

    Top-Down

--- practice_dp/practice_dp4.cpp
+++ practice_dp/practice_dp4.cpp
@@ -10,7 +10,20 @@
 using namespace std;
 
 // 네트워크 선 자르기 Top-Down : 재귀, 메모이제이션 
- 
+
+int dy[101]; // 메모이제이션  
+
+int DFS(int n){
+	
+	if(dy[n]>0) return dy[n];
+	if(n==1 || n==2) return n;
+	else return dy[n]=DFS(n-1)+DFS(n-2);
+}
 int main() {
 	
+	ios_base::sync_with_stdio(false);
+	int n;
+	cin>>n;
+	cout<<DFS(n);
+	return 0;
 }

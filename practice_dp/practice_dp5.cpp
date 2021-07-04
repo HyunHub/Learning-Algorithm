77413d9a61cbb4afdf730c3d881fb07cf861d7ae
HyunHub <19924___@naver.com>
2021-07-04 21:04:27 +0900

    LIS

---
+++ practice_dp/practice_dp5.cpp
@@ -0,0 +1,38 @@
+//#include <stdio.h>
+//#include <queue>
+//#include <vector>
+//#include <algorithm> 
+//#include <iostream>
+#include<bits/stdc++.h> 
+//#include <fstream>
+//#include <map>
+//#include <string> 
+using namespace std;
+
+// 최대 부분 증가수열
+// LIS : Longest Increasing Subsequence
+
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	int n, res=0;
+	cin>>n;
+	vector<int> arr(n+1), dy(n+1);
+	for(int i=1; i<=n; i++){
+		cin>>arr[i];
+	}
+	dy[1]=1;
+	for(int i=2; i<=n; i++){
+		int max=0;
+		for(int j=i-1; j>=1; j--){
+			if(arr[j]<arr[i] && dy[j]>max){
+				max=dy[j];
+			}
+		}
+		dy[i]=max+1;
+		if(dy[i]>res) res=dy[i];
+	}
+	
+	cout << res;
+	return 0;
+}

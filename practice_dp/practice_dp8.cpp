56d4798cdc69e0ec06cefe53005a4aeedac30a78
HyunHub <19924___@naver.com>
2021-07-10 15:08:04 +0900

    Top-Down

--- practice_dp/practice_dp8.cpp
+++ practice_dp/practice_dp8.cpp
@@ -9,4 +9,30 @@
 //#include <string> 
 using namespace std;
 
+// 알리바바와 40인의 도둑 Top-Down 
 
+int arr[21][21], dy[21][21];
+
+int DFS(int x, int y){
+	if(dy[x][y]>0) return dy[x][y];
+	if(x==0 && y==0) return arr[0][0];
+	else {
+		if(y==0) return dy[x][y] = DFS(x-1, y)+arr[x][y];
+		else if(x==0) return dy[x][y] =DFS(x, y-1)+arr[x][y];
+		else return dy[x][y] =min(DFS(x-1,y), DFS(x,y-1))+arr[x][y];
+	}
+}
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	int n;
+	cin>>n;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			cin>>arr[i][j];
+		}
+	}
+	
+	cout<<DFS(n-1, n-1);
+	return 0;
+}

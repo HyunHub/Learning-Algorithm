310f1144dc77ac3bc041f36302ea109e3bbddebf
HyunHub <19924___@naver.com>
2021-07-12 11:13:47 +0900

    dp9

--- practice_dp/practice_dp9.cpp
+++ practice_dp/practice_dp9.cpp
@@ -8,3 +8,21 @@
 //#include <map>
 //#include <string> 
 using namespace std;
+
+// 가방문제 (냅색 알고리즘)
+ 
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	int n, m, w, v;
+	cin>>n>>m;
+	vector<int> dy(m+1, 0);
+	for(int i=0; i<n; i++){
+		cin>>w>>v;
+		for(int j=w; j<=m; j++){
+			dy[j]=max(dy[j],dy[j-w]+v);
+		}
+	}
+	cout<<dy[m];
+	return 0; 
+}

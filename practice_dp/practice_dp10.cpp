1daddb175a7b42aef2569c602bb25c306a7baffb
HyunHub <19924___@naver.com>
2021-07-15 13:22:02 +0900

    Coin

--- practice_dp/practice_dp10.cpp
+++ practice_dp/practice_dp10.cpp
@@ -9,4 +9,25 @@
 //#include <string> 
 using namespace std;
 
-
+// 동전 교환 (냅색 알고리즘)
+ 
+int main() {
+	ios_base::sync_with_stdio(false);
+	int n, m;
+	cin>>n;
+	vector<int> coin(n);
+	for(int i=0; i<n; i++) cin>>coin[i];
+	cin>>m;
+	vector<int> dy(m+1, 1000);
+	dy[0]=0;
+	
+	for(int i=0; i<n; i++){
+		for(int j=coin[i]; j<=m; j++){
+			dy[j]=min(dy[j], dy[j-coin[i]]+1);
+		}
+	}
+	
+	cout<<dy[m];
+	
+	return 0;
+}
